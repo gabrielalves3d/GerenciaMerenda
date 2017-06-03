15b5f247a892b7017f6110c8832efcb19543c2d5
Gabriel Alves <gabrielalves3d@gmail.com>
2017-06-03 10:37:37 -0300

    Adicionando classes do modelo

---
+++ src/main/java/com/mycompany/gerenciarmerenda/modelo/Insumo.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.gerenciarmerenda.modelo;
+
+/**
+ *
+ * @author Gabriel Alves
+ */
+public class Insumo {
+    
+}
